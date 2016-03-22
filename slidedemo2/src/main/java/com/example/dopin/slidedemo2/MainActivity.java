dce74f55dfd9e761c96b5a71bdd2d2b79c0207d8
Dopins <1499286465@qq.com>
2016-03-22 17:01:26 +0800

    update

--- slidedemo2/src/main/java/com/example/dopin/slidedemo2/MainActivity.java
+++ slidedemo2/src/main/java/com/example/dopin/slidedemo2/MainActivity.java
@@ -9,23 +9,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> arrayList;
-    private DrawerLayout mDrawerLayout;
-    private ListView mListView;
-    private ArrayAdapter<String> adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mListView = (ListView) findViewById(R.id.drawer_content);
-        arrayList = new ArrayList<String>();
-        arrayList.add("1");
-        arrayList.add("1");
-        arrayList.add("1");
-        arrayList.add("1");
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
-        mListView.setAdapter(adapter);
+
     }
 }
