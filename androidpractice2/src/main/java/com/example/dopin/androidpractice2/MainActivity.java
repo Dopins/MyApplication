95a60731f3c9315125729c17c62439fb957f0118
Dopins <1499286465@qq.com>
2016-03-22 15:04:11 +0800

    update

--- androidpractice2/src/main/java/com/example/dopin/androidpractice2/MainActivity.java
+++ androidpractice2/src/main/java/com/example/dopin/androidpractice2/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dopin.androidpractice2;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
@@ -9,17 +11,21 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+    private  DrawerLayout mDrawerLayout;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
     }
     public void find(View view){
         Toast.makeText(this, "find", Toast.LENGTH_SHORT).show();
     }
     public void add(View view){
         Toast.makeText(this,"alert",Toast.LENGTH_SHORT).show();
+    }
+    public void menu(View view){
+        mDrawerLayout.openDrawer(Gravity.LEFT);
     }
 }
