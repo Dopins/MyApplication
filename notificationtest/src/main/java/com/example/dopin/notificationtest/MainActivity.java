4e91c0a83bfca493a953c4cf34b21b4fe72124c6
Dopins <1499286465@qq.com>
2016-03-22 23:31:02 +0800

    update

---
+++ notificationtest/src/main/java/com/example/dopin/notificationtest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.dopin.notificationtest;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private Button sendNotice;
+    private  Notification notification;;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sendNotice=(Button)findViewById(R.id.send_notice);
+        sendNotice.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.send_notice:
+                NotificationManager manager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                Notification.Builder builder = new Notification.Builder(this).setTicker("显示于屏幕顶端状态栏的文本")
+                        .setSmallIcon(R.mipmap.ic_launcher);
+                notification = builder.setContentIntent(null).setContentTitle("title").
+                        setContentText("text").build();
+                manager.notify(1, notification);
+                break;
+            default:
+                break;
+        }
+    }
+}
