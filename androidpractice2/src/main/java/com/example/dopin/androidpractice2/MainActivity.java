bd49fe82acd16b56b52c004d228e8ceeaefdfbae
Dopins <1499286465@qq.com>
2016-03-22 14:54:29 +0800

    update

--- androidpractice2/src/main/java/com/example/dopin/androidpractice2/MainActivity.java
+++ androidpractice2/src/main/java/com/example/dopin/androidpractice2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.dopin.androidpractice2;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -13,5 +15,11 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+    }
+    public void find(View view){
+        Toast.makeText(this, "find", Toast.LENGTH_SHORT).show();
+    }
+    public void add(View view){
+        Toast.makeText(this,"alert",Toast.LENGTH_SHORT).show();
     }
 }
