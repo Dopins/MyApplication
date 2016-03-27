251f47ef66e0c4095ba7a2a3a5e4ca30d0cbc1ae
Dopins <1499286465@qq.com>
2016-03-27 12:27:06 +0800

    update

--- androidtest/src/main/java/com/example/dopin/androidtest/MainActivity.java
+++ androidtest/src/main/java/com/example/dopin/androidtest/MainActivity.java
@@ -1,55 +1,80 @@
 package com.example.dopin.androidtest;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.BroadcastReceiver;
-import android.content.Context;
+
+import android.content.ComponentName;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.telephony.SmsManager;
-import android.telephony.SmsMessage;
-import android.widget.TextView;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends Activity {
 
-        private TextView sender;
-        private TextView content;
-        private IntentFilter receiveFilter;
-        private MessageReceiver messageReceiver;
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private Button startIntentService;
+    private Button bindService;
+    private Button unbindService;
+    private MyService.DownloadBinder downloadBinder;
+    private Button startService;
+    private Button stopService;
+    private ServiceConnection connection =new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder=(MyService.DownloadBinder)service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
          @Override
          protected void onCreate(Bundle savedInstanceState) {
-         super.onCreate(savedInstanceState);
-         setContentView(R.layout.activity_main);
-                sender=(TextView)findViewById(R.id.sender);
-                 content=(TextView)findViewById(R.id.content);
-                 receiveFilter=new IntentFilter();
-                 receiveFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
-                 messageReceiver=new MessageReceiver();
-                 registerReceiver(messageReceiver,receiveFilter);
+             super.onCreate(savedInstanceState);
+             setContentView(R.layout.activity_main);
+             startService=(Button)findViewById(R.id.start_service);
+             stopService=(Button)findViewById(R.id.stop_service);
+             startService.setOnClickListener(this);
+             stopService.setOnClickListener(this);
+             bindService=(Button)findViewById(R.id.bind_service);
+             unbindService=(Button)findViewById(R.id.unbind_service);
+             bindService.setOnClickListener(this);
+             unbindService.setOnClickListener(this);
+            startIntentService=(Button)findViewById(R.id.start_intent_service);
+             startIntentService.setOnClickListener(this);
          }
-        @Override
-        public void onDestroy(){
-                super.onDestroy();
-                unregisterReceiver(messageReceiver);
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.start_service:
+                Intent startIntent=new Intent(this,MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.stop_service:
+                Intent stopIntent=new Intent(this,MyService.class);
+                stopService(stopIntent);
+                break;
+            case R.id.bind_service:
+                Intent bindIntent =new Intent(this,MyService.class);
+                bindService(bindIntent,connection,BIND_AUTO_CREATE);
+                break;
+            case R.id.unbind_service:
+                unbindService(connection);
+                break;
+            case R.id.start_intent_service:
+                Log.d("tag","Thread id is "+Thread.currentThread().getId());
+                Intent intentService=new Intent(this,MyIntentService.class);
+                startService(intentService);
+                break;
+            default:
+                break;
         }
-        class MessageReceiver extends BroadcastReceiver{
-                @Override
-                public void onReceive(Context context,Intent intent){
-                        Bundle bundle=intent.getExtras();
-                        Object[] pdus=(Object[])bundle.get("pdus");
-                        SmsMessage[] messages=new SmsMessage[pdus.length];
-                        for(int i=0;i<messages.length;i++){
-                                messages[i]=SmsMessage.createFromPdu((byte[])pdus[i]);
-                        }
-                        String address=messages[0].getOriginatingAddress();
-                        String fullMessage="";
-                        for(SmsMessage message:messages){
-                                fullMessage+=message.getMessageBody();
-                        }
-                        sender.setText(address);
-                        content.setText(fullMessage);
-                }
-        }
+    }
+
 }
---
+++ androidtest/src/main/java/com/example/dopin/androidtest/MyIntentService.java
@@ -0,0 +1,26 @@
+package com.example.dopin.androidtest;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+
+
+public class MyIntentService extends IntentService {
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+       Log.d("tag", "Thread id is " + Thread.currentThread().getId());
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d("tag","onDestroy");
+    }
+}
